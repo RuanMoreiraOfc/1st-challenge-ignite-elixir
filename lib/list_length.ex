c9d307cd04a2b1c95ecbe54675db68d099adcc4b
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-06-27 13:10:00 -0300

    feat: return length of a list recursively

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,13 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) when is_list(list) do
+    increment_count(list, 0)
+  end
+
+  defp increment_count([], count) when is_integer(count) do
+    count + 0
+  end
+
+  defp increment_count([_head | tail], count) when is_integer(count) do
+    increment_count(tail, count + 1)
   end
 end
